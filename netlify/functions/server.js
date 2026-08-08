2e4bd3e07435e7bef75284524a24b16708042630
zzzx1of1 <96275038+zzzx1of1@users.noreply.github.com>
2026-08-08 16:48:44 -0400

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,1 +1,22 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const { Pool } = require('pg');
 
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.NEON_DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Bridge is running!', time: result.rows[0].now });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports.handler = serverless(app);
